cf50af333e81a5cb5d5065ad561fb1adccc9a681
AkhilaMandala <akhilamandala1111@gmail.com>
2025-09-14 22:19:55 +0530

    Time: 2 ms (98.9%), Space: 45.3 MB (10.25%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.java
@@ -0,0 +1,17 @@
+//find 2 elements in an array which gives the sum k 
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> numMap = new HashMap<>();
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            int complement = target - nums[i];
+            if (numMap.containsKey(complement)) {
+                return new int[]{numMap.get(complement), i};
+            }
+            numMap.put(nums[i], i);
+        }
+
+        return new int[]{}; // No solution found
+    }
+}
